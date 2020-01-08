42b12adf0adfac3e337c567e7cd31ef4d36331fa
Pavel Kiselyov <pavel@blockchain.com>
2020-01-08 15:15:48 +0000

    js example

--- app/js/main.go
+++ app/js/main.go
@@ -2,12 +2,24 @@ package main
 
 //go:generate gopherjs build --minify -o libcoins.js
 import (
-    "github.com/pavel-bc/libcoins/libcoins"
-    "github.com/gopherjs/gopherjs/js"
+	"fmt"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/pavel-bc/libcoins/libcoins"
 )
 
 func main() {
-    js.Global.Set("BitcoinSign", libcoins.BitcoinSign)
-    js.Global.Set("EthereumSign", libcoins.EthereumSign)
-    js.Global.Set("AlgoIsValidAddress", libcoins.AlgoIsValidAddress)
+	js.Global.Set("BitcoinSign", libcoins.BitcoinSign)
+	js.Global.Set("EthereumSign", libcoins.EthereumSign)
+	js.Global.Set("AlgoIsValidAddress", libcoins.AlgoIsValidAddress)
+
+	message := "Hello, world!"
+
+	btcecKey := "22a47fa09a223f2aa079edf85a7c2d4f8720ee63e502ee2869afab7de234b80c"
+	btcResult := libcoins.BitcoinSign(btcecKey, message)
+	fmt.Printf("Bitcoin signature: %s\n", btcResult.Unwrap())
+
+	ecdsaKey := "fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19"
+	ecdsaSig := libcoins.EthereumSign(ecdsaKey, message)
+	fmt.Printf("Ethereum signature: %s\n", ecdsaSig)
 }
